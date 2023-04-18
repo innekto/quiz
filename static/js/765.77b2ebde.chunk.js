7535afdc73d3cb30a40b19583bd2f6489e4d7ff2
innekto <110405105+innekto@users.noreply.github.com>
2023-04-18 11:58:59 +0000

    Deploying to gh-pages from @ innekto/quiz@3cde6379f1b9ba7f8e95ffe868f1e2a19f2847e4 🚀

---
+++ static/js/765.77b2ebde.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[765],{765:function(e,t,r){r.r(t);var n=r(87),s=(r(791),r(184));t.default=function(){return(0,s.jsxs)("main",{children:[(0,s.jsx)("h2",{children:"Mythology"}),(0,s.jsx)(n.rU,{to:"/questions",children:"Lets Go"})]})}}}]);
+//# sourceMappingURL=765.77b2ebde.chunk.js.map
